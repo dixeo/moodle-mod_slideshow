5951bc28641e73e254da49536972aee968650312
prismyq <pierre@prismyq.com>
2026-06-04 10:05:57 +0400

    Add release 1.0.0 and stable maturity

--- version.php
+++ version.php
@@ -27,4 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2026032813;        // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023100900;        // Moodle 4.3 (Build: 20231009) minimum.
 $plugin->component = 'mod_slideshow';   // Full name of the plugin (used for diagnostics).
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0.0';
 $plugin->cron      = 0;
