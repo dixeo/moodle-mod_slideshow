009392294b0a6fb75b7ec18c4ff9798152e19621
Josemaria Bolanos <josemabol@gmail.com>
2026-07-29 18:49:42 -0400

    Add release 1.1.0

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026032817;        // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2026072900;        // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2023100900;        // Moodle 4.3 (Build: 20231009) minimum.
 $plugin->component = 'mod_slideshow';   // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.0';
+$plugin->release   = '1.1.0';
 $plugin->cron      = 0;
